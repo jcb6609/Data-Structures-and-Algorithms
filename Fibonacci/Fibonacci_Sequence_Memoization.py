d0631ee56750453cda26e06aef1b96bc8f981086
Julio Cesar Bermudez Viatela <juliocbv2026@gmail.com>
2026-05-17 00:18:28 -0400

    Refactor Fibonacci sequence with memoization
    Refactored Fibonacci sequence implementation with memoization. Added comments for clarity on global variable usage.

--- Fibonacci/Fibonacci_Sequence_Memoization.py
+++ Fibonacci/Fibonacci_Sequence_Memoization.py
@@ -7,7 +7,7 @@ def main():
     for i in range(0, x + 1, 1):
         print(fibonacci(i), end=' ')
 
-fibonacciList = []  # global fibonacciList declaration 
+fibonacciList = []  # global fibonacciList declaration
 
 def fibonacci(n):
     if(n <= 1):
@@ -21,3 +21,10 @@ def fibonacci(n):
     return nthFibonacci
 
 main()
+
+# Comparison/Explanation:
+"""
+in Python, since variables inside functions are assumed to be local by default, then in order to use a outside function/not local variable
+(in this case the fibonaciList (a special variable) declaration), we need to appeal to the global keyword "global" followed by the name of 
+the variable to access the variable we want to use (fibonacciList) which is indeed outside function/not local, so that then it can be created).
+"""
